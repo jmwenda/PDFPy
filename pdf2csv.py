d52160d7f0824ea1be5759db763ad7b39a427385
Jude Mwenda <judemwenda@gmail.com>
2012-08-13 13:51:08 +0300

    Changes to allow for the running to be methodical

--- pdf2csv.py
+++ pdf2csv.py
@@ -1,13 +1,17 @@
 import argparse
+from lxml import etree
 
 parser = argparse.ArgumentParser(description='Tool for consuming xml"s produced by pdfminer')
 parser.add_argument("-f","--file",dest="xmlfile", type=file,help="Consume the xml file",metavar="PATH")
 
 args = parser.parse_args()
+def process_file():
+    if args.xmlfile:
+        #we now consume the xml file,first open then read
+        file = open(args.xmlfile.name,'r')
+        data = file.read()
+        root = etree.fromstring(data)
+        #we now cosume the formatted xml with lxml 
+    return root
+print process_file()
 
-if args.xmlfile:
-    #we now consume the xml file,first open then read
-    file = open(args.xmlfile.name,'r')
-    data = file.read() 
-    return data
-    
