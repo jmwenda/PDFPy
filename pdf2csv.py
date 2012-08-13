66ec6150dbfcb9cbc8363ef626c88131e78ab4ee
Jude Mwenda <judemwenda@gmail.com>
2012-08-13 16:04:30 +0300

    added list but need to find ameans to scheme the data better

--- pdf2csv.py
+++ pdf2csv.py
@@ -6,10 +6,25 @@ parser.add_argument("-f","--file",dest="xmlfile", type=file,help="Consume the xm
 
 args = parser.parse_args()
 
+def process_textlines(textbox):
+    textlines = textbox.getchildren()
+    lines = []
+    for textline in textlines:
+        texts = textline.getchildren()
+        cellvalue = ''
+        for text in texts:
+            cellvalue = cellvalue + text.text 
+            #cellbox = text.attrib['bbox']
+        lines.append(cellvalue)
+    return lines
 def process_page(page):
     #script that gets a page element and creates the csv
     page_bounds = page.attrib['bbox'] 
     textboxes = page.getchildren()
+    for textbox in textboxes:
+        textbox_bounds = textbox.attrib['bbox']
+        text_string = process_textlines(textbox)
+        print text_string
 def process_file():
     if args.xmlfile:
         #we now consume the xml file,first open then read
